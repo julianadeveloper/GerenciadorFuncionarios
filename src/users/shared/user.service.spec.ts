0f76c9ee668428d352fa677e47de12ab69775c49
Juliana Oliveira <juliana.oliveira@tallos.com.br>
2022-07-11 17:43:50 -0300

    npm install devido a bug no jest

--- src/users/shared/user.service.spec.ts
+++ src/users/shared/user.service.spec.ts
@@ -17,9 +17,31 @@ describe('userservice', () => {
     }).compile();
 
     provider = module.get<Userservice>(Userservice);
-  }).
+  })
 
   it('should be defined', () => {
     expect(provider).toBeDefined();
   });
+
+
 });
+
+
+// describe('UserController', () => {
+//   let usersController: UsersController;
+//   let usersService: Userservice;
+
+//   beforeEach(() => {
+//     // usersService = new usersService(usersController);
+//     usersController = new UsersController(usersService);
+//   });
+
+//   describe('findAll', () => {
+//     it('should return an array of cats', async () => {
+//       const result = ['test'];
+//       jest.spyOn(usersService, 'listUsers').mockImplementation(() => result['resultado ok']);
+
+//       expect(await usersController.listUsers(UsersController)).toBe(result);
+//     });
+//   });
+// });
