8f2465bb6cc33cc5ad907f10cbe5dfb960dfdc88
Juliana Oliveira <juliana.oliveira@tallos.com.br>
2022-07-14 17:58:38 -0300

    test- retorno listUsers/find

--- src/users/shared/1user.service.spec.ts
+++ src/users/shared/1user.service.spec.ts
@@ -6,7 +6,7 @@ import { createUser } from './dto/create-user.dto';
 import { User } from './user';
 import { Userservice } from './user.service';
 
-const userModelTest: User[] = [
+const userEntityList: User[] = [
 
   new User({
     _id: '89d58w5',
@@ -33,21 +33,21 @@ describe('userservice', () => {
 
   beforeEach(async () => {
     const userMockRepository = {
-      find: jest.fn().mockResolvedValue(userModelTest),
-      findAll: jest.fn().mockResolvedValue(userModelTest),
+      find: jest.fn().mockResolvedValue(userEntityList),
       findById: jest.fn(),
-      findOne: jest.fn().mockResolvedValue(userModelTest[0]),
-      registerUser: jest.fn().mockReturnValue(userModelTest[0]),
-      exec: jest.fn().mockImplementation(),
-      findByIdAndUpdat: jest.fn(),
-      findOneAndDelete: jest.fn(),
-      listUsers: jest.fn().mockImplementation(),
+      findOne: jest.fn(),
+      encodePwd: jest.fn(),
       create: jest.fn().mockResolvedValue( {
-        username: 'testUser1',
-        password: '123456',
-        name: 'teste1',
-        role: 'operador',
-      })
+      username: 'testUser1',
+      password: '123456',
+      name: 'teste1',
+      role: 'operador',
+      }),
+      emitnewUser: jest.fn(),
+      findByIdAndUpdate: jest.fn(),
+      emitupdateUser: jest.fn(),
+      findOneAndDelete: jest.fn(),
+      emitRemoveUser: jest.fn(),
     };
     const module: TestingModule = await Test.createTestingModule({
 
@@ -69,11 +69,28 @@ describe('userservice', () => {
   test('should be defined', () => {
     expect(userService).toBeDefined();
     expect(userRepository).toBeDefined();
-    console.log(userRepository)
   });
 
   // ListUser é uma função
-  
+  describe('findAll', () => {
+    it('UserList retorna uma função', async () => {
+      expect(userService.listUsers).toBe(userService.listUsers);
+   });
 
+   
+    // Se listUser Retorna um Array com dados do usuário
+    // it('Retorna com sucesso uma lista (array) de users', async () => {
+    //   const result = await userService.listUsers(async () => await [User]);
+    //   expect(result).toEqual(userModelTest);
+    //   //testando se meu user está sendo chamado pelo menos 1 vez.
+    //   expect(userRepository.find).toHaveBeenCalledTimes(1);
+    // });
 
-});
+    it('Retorna com sucesso uma lista (array) de users', async () => {
+      const result = await userService.listUsers([User]);
+      expect(result).toEqual(userEntityList);
+      //testando se meu user está sendo chamado pelo menos 1 vez.
+      expect(userRepository.find).toHaveBeenCalledTimes(1);
+    });
+  })}
+)
